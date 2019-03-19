edfeab19e1d8f81f25ccc5b3374f76c489c0d37a
goreleaserbot <goreleaser@carlosbecker.com>
2019-03-19 19:04:34 +0300

    Brew formula update for hookah version v0.2.3

--- Formula/hookah.rb
+++ Formula/hookah.rb
@@ -2,9 +2,9 @@
 class Hookah < Formula
   desc "Simple git hooks manager"
   homepage "https://github.com/Arkweid/hookah"
-  url "https://github.com/Arkweid/hookah/releases/download/v0.2.2/hookah_0.2.2_MacOS_x86_64.tar.gz"
-  version "0.2.2"
-  sha256 "97d17e009067a0e9dff84264268ba6c8af0251485b872028a35ae89970d6a153"
+  url "https://github.com/Arkweid/hookah/releases/download/v0.2.3/hookah_0.2.3_MacOS_x86_64.tar.gz"
+  version "0.2.3"
+  sha256 "3fe9ff9426ff3fe0607e778e0f06c93e96d033f2a621dd7e51ff99f551cdaccd"
 
   def install
     bin.install "hookah"
