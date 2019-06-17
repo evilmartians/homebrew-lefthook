63bd0ee1e0d0e813c98a80f42fba9a4c4de8baef
goreleaserbot <goreleaser@carlosbecker.com>
2019-06-17 18:17:49 +0300

    Brew formula update for lefthook version v0.5.3

--- Formula/lefthook.rb
+++ Formula/lefthook.rb
@@ -2,9 +2,9 @@
 class Lefthook < Formula
   desc "Lefthook a single dependency-free binary to manage all your git hooks that works with any language in any environment, and in all common team workflows"
   homepage "https://github.com/Arkweid/lefthook"
-  url "https://github.com/Arkweid/lefthook/releases/download/v0.5.2/lefthook_0.5.2_MacOS_x86_64.tar.gz"
-  version "0.5.2"
-  sha256 "1003f3195dd4c1c4f88e3ac6d97ef893b85046ecbefd35f58ab72393007dd680"
+  url "https://github.com/Arkweid/lefthook/releases/download/v0.5.3/lefthook_0.5.3_MacOS_x86_64.tar.gz"
+  version "0.5.3"
+  sha256 "a0bcaa8a40624b913eec6bd088828b39499cf349b161e14934cdc6f85d754071"
 
   def install
     bin.install "lefthook"
