e3f74d5766533f1a99eb77736bcf587bce4493dd
Abroskin Alexander <arkweid@evilmartians.com>
2020-02-02 15:55:31 +0300

    Update lefthook.rb

--- Formula/lefthook.rb
+++ Formula/lefthook.rb
@@ -2,9 +2,9 @@
 class Lefthook < Formula
   desc "Lefthook a single dependency-free binary to manage all your git hooks that works with any language in any environment, and in all common team workflows"
   homepage "https://github.com/Arkweid/lefthook"
-  url "https://github.com/Arkweid/lefthook/releases/download/v0.7.0/lefthook_0.7.0_MacOS_x86_64.gz"
-  version "0.7.0"
-  sha256 "27de4b58f1915d425eacc6fbb6fee469114d2c2e7395af330c6c29752da2c269"
+  url "https://github.com/Arkweid/lefthook/releases/download/v0.7.1/lefthook_0.7.1_MacOS_x86_64.gz"
+  version "0.7.1"
+  sha256 "02f308d72dd09997180380013f50734e9202f9a9f45346d98284d93a17c394d6"
 
   def install
     bin.install "lefthook"
