31f752e97543d5048436b2dc58dda3a92e177496
Andrey Novikov <envek@envek.name>
2021-04-30 18:08:17 +0300

    Deprecate this tap as formula has been added to Homebrew Core

--- Formula/lefthook.rb
+++ Formula/lefthook.rb
@@ -5,6 +5,8 @@ class Lefthook < Formula
   sha256 "f4247621abd7ee9860d8c18d6357077978f09e439bde9ff16462e3e743a93ee9"
   license "MIT"
 
+  deprecate! date: "2021-04-25", because: "has been accepted to Homebrew Core. Use `brew install lefthook` instead"
+
   bottle do
     root_url "https://github.com/evilmartians/homebrew-lefthook/releases/download/lefthook-0.7.4"
     sha256 cellar: :any_skip_relocation, catalina:     "88a4c54f3de5c09297fda72c17a29dce639d0bad329d01d3520c2b732ad8a308"
